8a9e34ba7ce3b5bcf5b44fdf4ed19eb471c6f5af
Ілона Мельник <66883153+TimofeyMaryin@users.noreply.github.com>
2024-09-09 22:49:42 +0500

    Add GuestUserParameters

--- app/src/main/java/com/broken/link/buster/data/_const/AppSettingsInfo.kt
+++ app/src/main/java/com/broken/link/buster/data/_const/AppSettingsInfo.kt
@@ -3,3 +3,11 @@ package com.broken.link.buster.data._const
 const val webClientId = "886930288472-cenrat278g32fpgm3bjp9kee3dd1egh1.apps.googleusercontent.com"
 const val SHARED_USER_STATUS_NAME = "shared_user_data_name" // название БД
 const val USER_STATUS_SHARED_NAME = "user_status_shared_name" // имя для поля статуса юзера
+
+
+object GuestUserParameters {
+    const val SHARED_PREF_NAME = "shared_pref_name"
+    const val SHARED_PREF_ABOUT = "shared_pref_about"
+    const val SHARED_PREF_IMG_INDEX = "shared_pref_img_index"
+    const val SHARED_PREF_COLOR_BORDER_INDEX = "shared_pref_color_border_index"
+}
